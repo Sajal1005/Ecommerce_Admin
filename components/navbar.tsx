9e1f76b342d7d01f6af3230b740d6d8984d79e98
Sajal1005 <nandasajal.208@gmail.com>
2023-07-18 12:11:29 +0530

    built navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,8 @@
 import { UserButton, auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import StoreSwitcher from "@/components/store-switcher";
-// import { MainNav } from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
 // import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
 
@@ -22,8 +22,8 @@ const Navbar = async () => {
   return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        {/* <StoreSwitcher items={stores} />
-        <MainNav className="mx-6" /> */}
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           {/* <ThemeToggle /> */}
           <UserButton afterSignOutUrl="/" />
